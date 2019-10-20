99fc5966384faa865b72b4f1e604b8dcd791cbee
ngocdangrby <ngocdang8a@gmail.com>
2019-10-20 17:31:57 +0700

    regex price || edit website regex || add tokenizer

--- pre-process/convert_to_csv.py
+++ pre-process/convert_to_csv.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pre_process
-
+from sklearn.model_selection import train_test_split
+import crf_tokenizer
 
 SELL = '__label__post_ban_hang'
 NON_SELL = '__label__post_khong_ban'
@@ -10,16 +11,29 @@ non_sell_ls = []
 #read data line by line and split 
 lines = [line.rstrip('\n') for line in open('../data/data_original.txt')]
 
+# for l in lines:
+#     line = l.split(" ", 1)
+#     if line[0] == SELL:
+#         sell_ls.append(pre_process.pre_process(line[1]))
+#     elif line[0] == NON_SELL:
+#         non_sell_ls.append(pre_process.pre_process(line[1]))
+
+
 for l in lines:
     line = l.split(" ", 1)
     if line[0] == SELL:
-        sell_ls.append(pre_process.pre_process(line[1]))
+        sell_ls.append(line[1])
     elif line[0] == NON_SELL:
-        non_sell_ls.append(pre_process.pre_process(line[1]))
+        non_sell_ls.append(line[1])
+
+pr = lambda x: pre_process.pre_process(x)
+sell_ls = [pr(x) for x in sell_ls]
+non_sell_ls = [pr(x) for x in non_sell_ls]
 
 #write to file
 df = pd.DataFrame(sell_ls)
 df.to_csv("../data/sell.csv", header=False, index=False)
+
 
 df = pd.DataFrame(non_sell_ls)
 df.to_csv("../data/nonsell.csv", header=False, index=False)
--- pre-process/pre_process.py
+++ pre-process/pre_process.py
@@ -1,4 +1,10 @@
 import re
+import crf_tokenizer
+from crf_tokenizer import CrfTokenizer
+
+crf_tokenizer_obj = CrfTokenizer()
+# crf_tokenizer_obj.train('../data/tokenized/samples/training')
+
 def remove_emoji(text):
     emoji_pattern = re.compile("["
                            u"\U0001F600-\U0001F64F"  # emoticons
@@ -7,28 +13,43 @@ def remove_emoji(text):
                            u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                            u"\U00002702-\U000027B0"
                            u"\U000024C2-\U0001F251"
+                           u'\U0001F900-\U0001F999'
                            "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
 
 def remove_common_sign(text):
-    pattern = "[-.,=()"":*!?<>;|,^]"
+    pattern = r'[-.,=()\\"\'/:*&%!?<>;|,^+[~\]]'
     return re.sub(pattern," ", text)
 
 def remove_phone_number(text):
     
-    phone_pattern = "\(?(?:\+62|62|0)(?:\d{2,3})?\)?[ .-]?\d{2,4}[ .-]?\d{2,4}[ .-]?\d{2,4}"
-    return re.sub(phone_pattern, 'sdt', text)
+    phone_pattern = "\(?(?:\+84|84|0)(?:\d{2,3})?\)?[ .-]?\d{2,4}[ .-]?\d{2,4}[ .-]?\d{2,4}"
+    return re.sub(phone_pattern, 'số_điện_thoại', text)
 
 def remove_website_link(text):
-    website_pattern = "http\S+"
-    return re.sub(website_pattern, "website", text.strip())
-    
-def pre_process(text):
-    text = remove_website_link(text)
-    text = remove_emoji(text)
-    text = remove_common_sign(text)
-    text = remove_phone_number(text)
-    text = text.lower()
-    return text
+    incorrect_prefix = ["http : //", "http: //", "https: //", "https : //"]
+    for x in incorrect_prefix:
+        text = text.replace(x, "http://")
+    website_pattern = "(http\S+|bit.ly\S+|goo.gl\S+|tinyurl.com\S+)"
+    return re.sub(website_pattern, "link_website", text.strip())
 
+def remove_price(text):
+    price_pattern = r"(#\b(\d+\S+|\d+)(k|vnd|vnđ|đ|d|m)\b|\b(\d+\S+|\d+)tr\b|\b\d+(k|vnd|vnđ|đ|d|m)\b)"
+    return re.sub(price_pattern, "giá_sản_phẩm", text)
+
+def tokenizer_using_crf(text):
+    tokenized_sent = crf_tokenizer_obj.get_tokenized(text)
+    return tokenized_sent
+    # tokens = crf_tokenizer_obj.tokenize(text)
+    # print(tokens)
+
+def pre_process(text):
+    text = text.lower()
+    text = remove_emoji(text)
+    text = remove_price(text)
+    text = remove_website_link(text)
+    text = remove_phone_number(text)
+    text = remove_common_sign(text)
+    text = tokenizer_using_crf(text)
+    return text
 
