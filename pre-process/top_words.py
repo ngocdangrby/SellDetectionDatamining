c031b2c1f4dfae2bb92dc5a0fbf313e11be36a9f
ngocdangrby <ngocdang8a@gmail.com>
2019-10-30 22:17:49 +0700

    updatedetails

--- pre-process/top_words.py
+++ pre-process/top_words.py
@@ -11,8 +11,8 @@ def top_words(data, stopwords = [], file_name = "chart", to_print = 20):
                 wordcount[word] += 1
           
     mc = sorted(wordcount.items(), key=lambda k_v: k_v[1], reverse=True)[:to_print]
-    for word, count in mc:
-        print(word, ":", count)
+    # for word, count in mc:
+    #     print(word, ":", count)
 
     mc = dict(mc)
     names = list(mc.keys())
@@ -22,40 +22,42 @@ def top_words(data, stopwords = [], file_name = "chart", to_print = 20):
     plt.savefig(file_name + ".png")
     plt.show()
 
-def two_top_words(data1, data2, stopwords = [], file_name = "chart", to_print = 20):
+def two_top_words(data, stopwords = [], file_name = "chart", to_print = 20):
     wordcount = collections.defaultdict(int)
+    data1 = list((data['content'].where(data['label'] == 0)).dropna())
+    data2 = list((data['content'].where(data['label'] == 1)).dropna())
+ 
+ 
     for line in data1:
         for word in str(line).lower().split():
             if word not in stopwords:
                 wordcount[word] += 1
           
     mc = sorted(wordcount.items(), key=lambda k_v: k_v[1], reverse=True)[:to_print]
-    for word, count in mc:
-        print(word, ":", count)
 
     mc = dict(mc)
     names = list(mc.keys())
     values = list(mc.values())
- 
+    plt.title("Top words bán hàng")
     plt.barh(range(len(mc)),values,tick_label=names)
     plt.savefig(file_name + "_1.png")
     plt.show()
 
 
+
     wordcount = collections.defaultdict(int)
-    for line in data1:
+    for line in data2:
         for word in str(line).lower().split():
             if word not in stopwords:
                 wordcount[word] += 1
           
     mc = sorted(wordcount.items(), key=lambda k_v: k_v[1], reverse=True)[:to_print]
-    for word, count in mc:
-        print(word, ":", count)
 
     mc = dict(mc)
     names = list(mc.keys())
     values = list(mc.values())
     # plt.subplot(1,1,2)
+    plt.title("Top words KHÔNG bán hàng")
     plt.barh(range(len(mc)),values,tick_label=names)
     plt.savefig(file_name + "_2.png")
     plt.show()
